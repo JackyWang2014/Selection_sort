4a1a9c5c605ee8a5efb1c718d0a58a4e89c84520
JackyWang2014 <339847016@qq.com>
2016-04-16 10:51:24 +0800

    Selection_sort

--- Selection_sort/main.c
+++ Selection_sort/main.c
@@ -9,7 +9,26 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    int a[10] = {5,1,3,9,7,6,4,2,0,8};
+    
+    for (int i = 0; i<10; i++) {
+        printf("%d\n",a[i]);
+    }
+    
+    for (int i = 0; i < 10 - 1; i++) {
+        for (int j = i+1; j < 10; j++) {
+            if (a[i] > a[j]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    
+    for (int i = 0; i<10; i++) {
+        printf("%d\n",a[i]);
+    }
+    
     return 0;
 }
